abf6a3d8023ebb944e76fdaa584c55ac2cf70444
Simon Josi <simon.josi@atizo.com>
2010-11-12 22:22:28 +0100

    add new copyright notice; switch to two space indentation; switch to class parameters

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,8 +3,9 @@
 #
 # Copyright 2008, admin(at)immerda.ch
 # Copyright 2008, Puzzle ITC GmbH
+# Copyright 2010, Atizo AG
 # Marcel Härry haerry+puppet(at)puzzle.ch
-# Simon Josi josi+puppet(at)puzzle.ch
+# Simon Josi simon.josi+puppet(at)atizo.com
 #
 # This program is free software; you can redistribute 
 # it and/or modify it under the terms of the GNU 
@@ -12,29 +13,30 @@
 # the Free Software Foundation.
 #
 
-class resolvconf {
-    case $operatingsystem {
-        openbsd: {
-            info('$resolvconf_domain and $resolvconf_search not needed on openbsd')
-        }
-        default: {
-            if ! $resolvconf_domain {
-                fail("you need to define \$resolvconf_domain for ${fqdn}")
-            }
-            if ! $resolvconf_search {
-                fail("you need to define \$resolvconf_search for $fqdn")
-            }
-        }
+class resolvconf(
+  $resolv_domain = false,
+  $resolv_search = false,
+  $resolv_nameservers = false
+){
+  if $operatingsystem == 'openbsd' {
+    info('$resolv_domain and $resolv_search not needed on openbsd')
+  } else {
+    if ! $resolv_domain {
+      fail("you need to define \$resolv_domain for $fqdn")
     }
-    if ! $resolvconf_nameservers {
-        fail("you need to define \$resolvconf_nameservers for $fqdn")
+    if ! $resolv_search {
+      fail("you need to define \$resolv_search for $fqdn")
     }
-    file{'/etc/resolv.conf':
-        path => '/etc/resolv.conf',
-        owner => root, group => 0, mode => 0444,
-        content => $operatingsystem ? {
-            openbsd => template("resolvconf/resolvconf.$operatingsystem.erb"),
-            default => template('resolvconf/resolvconf.erb'),
-        }
+  }
+  if ! $resolv_nameservers {
+    fail("you need to define \$resolv_nameservers for $fqdn")
+  }
+  file{'/etc/resolv.conf':
+    path => '/etc/resolv.conf',
+    owner => root, group => 0, mode => 0444,
+    content => $operatingsystem ? {
+      openbsd => template("resolvconf/resolvconf.$operatingsystem.erb"),
+      default => template('resolvconf/resolvconf.erb'),
     }
+  }
 }
